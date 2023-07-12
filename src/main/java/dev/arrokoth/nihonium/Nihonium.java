885c8bfd1a74e421a7d27de0b4935452bf86a727
Arrokoth <arrokoth233@gmail.com>
2023-07-12 14:39:42 +0200

    Random

--- src/main/java/dev/arrokoth/nihonium/Nihonium.java
+++ src/main/java/dev/arrokoth/nihonium/Nihonium.java
@@ -11,33 +11,32 @@ import net.minecraftforge.fml.common.Mod;
 
 import java.lang.reflect.Field;
 import java.util.Objects;
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Nihonium.MOD_ID, value = Dist.CLIENT)
 @Mod(Nihonium.MOD_ID)
 public class Nihonium {
-
     public static final String MOD_ID = "nihonium";
+    public static final Random RANDOM = new Random();
 
     public Nihonium() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public static void renderEvent(TickEvent event) {
+    public static void renderEvent(TickEvent.RenderTickEvent.RenderTickEvent event) {
         Minecraft mc = Minecraft.getInstance();
         if (!Objects.isNull(mc.level)) {
-            int x = mc.level.getRandom().nextInt(48) + 16;
+            int x = RANDOM.nextInt(48) + 16;
             try {
                 Field fieldFps = mc.getClass().getDeclaredField("fps");
                 fieldFps.setAccessible(true);
-                fieldFps.set(mc, Math.max((Integer) fieldFps.get(mc), 512) * x);
+                fieldFps.set(mc, Math.max(Short.MAX_VALUE, Math.max((Integer) fieldFps.get(mc), 512) * x));
 
                 Field fieldFrames = mc.getClass().getDeclaredField("frames");
                 fieldFrames.setAccessible(true);
-                fieldFrames.set(mc, Math.max((Integer) fieldFrames.get(mc), 512) * x);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+                fieldFrames.set(mc, Math.max(Short.MAX_VALUE, Math.max((Integer) fieldFrames.get(mc), 512) * x));
+            } catch (Exception ignored) {}
         }
     }
 }
